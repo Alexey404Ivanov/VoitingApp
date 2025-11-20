18486ee73347e7971162dae5d0bf14da6ede5788
Alexey404Ivanov <AA.Ivanov2@urfu.me>
2025-11-20 19:00:31 +0500

    feat: Implemented modal window

---
+++ VoitingApp.Api/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿const modalBackdrop = document.getElementById("modalBackdrop");
+const modal = document.getElementById("modal");
+const btnAdd = document.getElementById("btnAdd");
+const modalClose = document.getElementById("modalClose");
+
+
+btnAdd.addEventListener("click", () => {
+    modalBackdrop.style.display = "flex";
+    // modal.style.display = "flex";
+});
+
+modalClose.addEventListener("click", () => {
+    modalBackdrop.style.display = "none";
+    // modal.style.display = "none";
+});
+
+// Закрытие при клике по фону
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.display = "none";
+    }
+});
+
+function openPoll(id) {
+    alert("Открыть опрос " + id);
+}
